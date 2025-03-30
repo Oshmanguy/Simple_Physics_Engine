933bfb8129df0223b6c98a2ec258320ba60559fb
Josh Agrey <joshagrey1@gmail.com>
2025-03-29 20:42:24 -0600

    finished creating physicObject class

--- src/PhysicsObject.java
+++ src/PhysicsObject.java
@@ -8,10 +8,27 @@ public class PhysicsObject {
     double bounce = 0.8;
     int size = 20;
 
+    /**
+     * CONSTRUCTOR
+     *
+     * @param x - x location
+     * @param y - y location
+     */
     public PhysicsObject(double x, double y) {
         this.x = x;
         this.y = y;
-        this.vx = 2;
-        this.vy = 0;
+        this.vx = 2; //Initial velocity in x direction
+        this.vy = 0; //Initial velocity in y direction
+    }
+
+    public void update() {
+        vy = vy + gravity; //Applying gravity
+        x = x + vx;
+        y = y + vy;
+
+        if (y + size > 400) { //Collision with ground
+            y = 400 - size;
+            vy = vy * -bounce; //Bounce effect
+        }
     }
 }
